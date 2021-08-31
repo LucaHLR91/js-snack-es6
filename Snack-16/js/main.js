28dd03a0a05f81528fcf1afc97fde3c051772d74
LucaHLR <mastrantonio.luca.91@icloud.com>
2021-08-31 12:41:41 +0200

    array

---
+++ Snack-16/js/main.js
@@ -0,0 +1,19 @@
+// Crea 10 oggetti che rappresentano una zucchina, 
+//  indicandone per ognuno varietà, peso e lunghezza. 
+//  Calcola quanto pesano tutte le zucchine.
+
+// CREO ARRAY DI ZUCCHINE
+const zucchine = [
+    {varieta: 'romana', peso: 1, lunghezza: 1, },
+    {varieta: 'striata', peso: 3, lunghezza: 2, },
+    {varieta: 'rigata', peso: 2, lunghezza: 3, },
+    {varieta: 'romana', peso: 5, lunghezza: 4, },
+    {varieta: 'tonda', peso: 1, lunghezza: 1, },
+    {varieta: 'romana', peso: 7, lunghezza: 2, },
+    {varieta: 'striata', peso: 3, lunghezza: 6, },
+    {varieta: 'rigata', peso: 5, lunghezza: 8, },
+    {varieta: 'romana', peso: 8, lunghezza: 6, },
+    {varieta: 'romana', peso: 6, lunghezza: 8, },
+];
+
+// CALCOLO IL PESO TOTALE DELLE ZUCCHINE
