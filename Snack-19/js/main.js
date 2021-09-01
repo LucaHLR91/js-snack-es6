738da0bded6c27235375091da85c98d66f688742
LucaHLR <mastrantonio.luca.91@icloud.com>
2021-09-01 17:36:57 +0200

    Snack 19 terminated

--- Snack-19/js/main.js
+++ Snack-19/js/main.js
@@ -1,3 +1,34 @@
 // Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
 // La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
 // Usiamo i nuovi metodi degli array foreach o filter.
+
+// SCRIVO LA FUNZIONE
+let numbers = [85, 104, 41, 33, 76, 58, 88, 95];
+let numberA = 3; // RAPPRESENTANO LA POSIZIONE NELL'ARRAY
+let numberB = 5; // RAPPRESENTANO LA POSIZIONE NELL'ARRAY
+
+const myFunction = (array, numA, numB) => {
+    let newNumbers =[];
+    array.forEach((element, index) => {
+        if (index >= numA && index <= numB) {
+            newNumbers.push(element);
+        } 
+    });
+    return newNumbers;
+};
+
+console.log(myFunction(numbers, numberA, numberB));
+
+// SCRIVO LA FUNZIONA CON IL FILTER
+
+const miaFunzione = (array, numA, numB) => {
+    let nuoviNumeri = [];
+    let numero = array.filter((numero, index) => {
+        if (index >= numA && index <= numB) {
+            nuoviNumeri.push(numero);
+        }
+    });
+    return nuoviNumeri;
+};
+
+console.log(miaFunzione(numbers, numberA, numberB));
